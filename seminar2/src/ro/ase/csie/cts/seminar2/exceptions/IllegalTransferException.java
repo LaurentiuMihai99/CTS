7d65a9f41cc88f0718da3710535f611dcc85e19b
LaurentiuMihai99 <naum_mihai2008@yahoo.com>
2021-03-11 13:49:39 +0200

    Initial Seminar3 Commit

--- seminar2/src/ro/ase/csie/cts/seminar2/exceptions/IllegalTransferException.java
+++ seminar2/src/ro/ase/csie/cts/seminar2/exceptions/IllegalTransferException.java
@@ -1,5 +1,5 @@
 package ro.ase.csie.cts.seminar2.exceptions;
 
-public class IllegalTransferException extends Exception{
+public class IllegalTransferException extends Exception {
 	
 }
--- seminar2/src/ro/ase/csie/cts/seminar2/interfaces/Profitable.java
+++ seminar2/src/ro/ase/csie/cts/seminar2/interfaces/Profitable.java
@@ -1,5 +1,6 @@
 package ro.ase.csie.cts.seminar2.interfaces;
 
 public interface Profitable {
-
+	public static final double MAX_PERCENT_RATE = 4;
+	public abstract void addInterest(double interestPercentRate);
 }
